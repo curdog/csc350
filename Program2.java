4c602389b8e602e19c19837cda1e66980a6ac56d
curdog <curdog@gmail.com>
2015-02-09 11:06:04 -0500

    merg2

--- Program2.java
+++ Program2.java
@@ -26,6 +26,13 @@ class Program2 {
 <<<<<<< HEAD
 		Hashtable<Integer, Word> dictionary = new Hashtable<Integer, Word>();
 
+=======
+		Hashtable<Integer, Word> dictionary = new Hashtable<Integer,Word>();
+		
+		//printwriter tp wrote to the output file
+		PrintWriter pw = new PrintWriter(output);
+		
+>>>>>>> origin/master
 		// get filename
 		do {
 			// no arguments were entered in the command line
@@ -97,6 +104,14 @@ class Program2 {
 			String token = inline.nextToken();
 
 			// test string for integer
+=======
+			} // end while
+			
+			//printing to the output file with PrintWriter(pw)
+			pw.println("The sum of integers is: " + sum);
+			
+			// close input file
+>>>>>>> origin/master
 			try {
 				sum += Integer.parseInt(token);
 			} catch (NumberFormatException e) {
