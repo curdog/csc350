8014ed2cd09dc51271a7a9ad386c9ca52da9f5bd
curdog <curdog@gmail.com>
2015-02-11 17:09:59 -0500

    Starting Code
    Event Listeners and such to get a minimally viable program

--- src/Program3.java
+++ src/Program3.java
@@ -1,10 +1,81 @@
 package src;
 
-public class Program3 {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.Frame;
 
+
+//assignment 3
+//csc java
+//Laurel Cris Sean Group 3
+//
+
+class Main extends Frame implements ActionListener, WindowListener {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Main wind = new Main();
+		wind.setVisible( true);
+		
 
 	}
+	
+	public Main(){
+		
+		this.addWindowListener( this );
+		this.setResizable(false);
+			
+		
+	}
 
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
