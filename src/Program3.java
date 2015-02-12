c402fc0fde14999ec65a1f6ce686fe360c5a2800
curdog <curdog@gmail.com>
2015-02-12 14:27:31 -0500

    GUI setup
    mostly complete

--- src/Program3.java
+++ src/Program3.java
@@ -4,8 +4,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.awt.Button;
 import java.awt.Frame;
-
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Label;
+import java.awt.List;
+import java.awt.TextField;
+import java.io.File;
 
 //assignment 3
 //csc java
@@ -14,6 +20,19 @@ import java.awt.Frame;
 
 class Main extends Frame implements ActionListener, WindowListener {
 	
+	GridBagLayout gbl;
+	GridBagConstraints gbc;
+	Button ok;
+	Button srcSelect;
+	List fileListing;
+	
+	Label src;
+	Label mesg;
+	Label targetBase;
+	
+	TextField targetName;
+	
+	File curDir;
 	public static void main(String[] args) {
 		Main wind = new Main();
 		wind.setVisible( true);
@@ -23,10 +42,44 @@ class Main extends Frame implements ActionListener, WindowListener {
 	
 	public Main(){
 		
+		curDir = new File( "." );
+		gbl = new GridBagLayout();
+		gbc = new GridBagConstraints();
+		
 		this.addWindowListener( this );
 		this.setResizable(false);
-			
+		this.setLayout(gbl);
 		
+		fileListing = new List();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth =2;
+		gbc.ipady = 200;
+		this.add( fileListing, gbc );
+		
+		for( int i = 0; i < curDir.listFiles().length; i++ ){
+			fileListing.add( curDir.listFiles()[i].getName() );
+		}
+		
+		gbc.ipady=0;
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridwidth = 1;
+		this.add( srcSelect, gbc );
+		
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		this.add( src, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		this.add( ok, gbc );
+		
+		gbc.gridx = 1;
+		this.add(targetBase);
+		
+		gbc.gridx = 2;
+		this.add(targetName);
 	}
 
 	@Override
@@ -62,7 +115,7 @@ class Main extends Frame implements ActionListener, WindowListener {
 	@Override
 	public void windowIconified(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+		this.setState(Frame.NORMAL);
 	}
 
 	@Override
